6798f124415e7e5cd24fe859732152cd8b4fa551
Louis Acresti <louis.acresti@gmail.com>
2019-04-16 11:19:13 -0700

    Document config-overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,17 +1,34 @@
 const fs = require("fs");
 
 module.exports = function override(config, env) {
-  config.module.rules.unshift({
-    test: /.pc$/,
-    use: [
-      {
-        loader: "paperclip-react-loader",
-        options: {
-          config: JSON.parse(fs.readFileSync("./app.tdproject", "utf8"))
+  const oldRules = config.module.rules;
+  config.module.rules = [
+    ...oldRules.slice(0, oldRules.length - 1),
+
+    // Inject the paperclip loader _just_ before the last rule. If we put our
+    // rule at the very end of the rules list, the `file-loader` rule will
+    // gobble up our `.pc` files before our loader can get to it.
+    //
+    // TODO: How can we get hot reloading working with `.pc` files?
+    //
+    // Right now, you have to manually refresh; changing a JS file triggers the
+    // refresh for you. It shouldn't be hard to achieve this basic behavior for
+    // `.pc` files, I think.
+    {
+      test: /.pc$/,
+      use: [
+        {
+          loader: "paperclip-react-loader",
+          options: {
+            config: JSON.parse(fs.readFileSync("./app.tdproject", "utf8"))
+          }
         }
-      }
-    ]
-  });
+      ]
+    },
+
+    // This _should_ be the `file-loader` rule:
+    config.module.rules[config.module.rules.length - 1]
+  ];
 
   return config;
 };
